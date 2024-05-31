cb7a2c1567df233bb4c385488e9ab9fcc1e63d79
mtaku3 <m.taku3.1222@gmail.com>
2024-06-01 00:39:26 +0900

    fix: remove debug flag of none-ls

--- modules/home/app/dev/neovim/config/lua/plugins/none-ls.lua
+++ modules/home/app/dev/neovim/config/lua/plugins/none-ls.lua
@@ -4,7 +4,6 @@ return {
     dir = "@none_ls_nvim@",
     config = function()
       require("null-ls").setup({
-        debug = true,
         on_attach = function(_, bufnr)
           vim.keymap.set("n", "<leader>f", function()
             vim.lsp.buf.format({ name = "null-ls", timeout_ms = 10000 })
