076f90c12183d8a0ded27a679096ca1a549f58c1
mtaku3 <m.taku3.1222@gmail.com>
2024-05-31 16:02:15 +0900

    fix: disable devenv cachix

--- devenv.nix
+++ devenv.nix
@@ -16,4 +16,6 @@
   scripts.cleanup.exec = "rm *.qcow2 || true";
   scripts.dev.exec = "cleanup && nix flake check && $(nixos-generate -f vm --flake \".?submodules=1#$1\" | tail -n 1)";
   scripts.switch.exec = "nixos-rebuild switch --flake \".?submodules=1#$1\"";
+
+  cachix.enable = false;
 }
