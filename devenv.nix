3e0772465488e800148c676cd68101772c78e377
mtaku3 <m.taku3.1222@gmail.com>
2024-05-31 13:52:21 +0900

    chore: add some commands to devnev

--- devenv.nix
+++ devenv.nix
@@ -9,7 +9,11 @@
     husky
     lint-staged
     ormolu
+    nixos-generators
   ];
 
   scripts.fmt.exec = "nix fmt && ormolu --mode inplace $(find . -name '*.hs')";
+  scripts.cleanup.exec = "rm *.qcow2 || true";
+  scripts.dev.exec = "cleanup && nix flake check && $(nixos-generate -f vm --flake \".?submodules=1#$1\" | tail -n 1)";
+  scripts.switch.exec = "nixos-rebuild switch --flake \".?submodules=1#$1\"";
 }
