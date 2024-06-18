0ebf040bb82e63bd592017a18accfebbbb6348aa
mtaku3 <m.taku3.1222@gmail.com>
2024-06-18 15:30:15 +0900

    chore: fix dev command

--- devenv.nix
+++ devenv.nix
@@ -20,7 +20,7 @@
 
   scripts.fmt.exec = "nix fmt && ormolu --mode inplace $(find . -name '*.hs')";
   scripts.cleanup.exec = "rm *.qcow2 || true";
-  scripts.dev.exec = "cleanup && nix flake check && $(nixos-generate -f vm --flake \".?submodules=1#$1\" | tail -n 1)";
+  scripts.dev.exec = "cleanup && $(nix flake check || true) && $(nixos-generate -f vm --flake \".?submodules=1#$1\" | tail -n 1)";
   scripts.switch.exec = "nixos-rebuild switch --flake \".?submodules=1#$1\"";
 
   cachix.enable = false;
