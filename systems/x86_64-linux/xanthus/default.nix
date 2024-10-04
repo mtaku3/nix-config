7a426045472d49c9be8e6bc0dc677b85b74bb055
mtaku3 <m.taku3.1222@gmail.com>
2024-10-05 01:56:42 +0900

    feat: add new host, xanthus

---
+++ systems/x86_64-linux/xanthus/default.nix
@@ -0,0 +1,34 @@
+{lib, ...}:
+with lib;
+with lib.capybara; {
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  capybara = {
+    system.bluetooth = enabled;
+    archetypes.workstation = enabled;
+
+    app = {
+      system = {
+        blueman = enabled;
+      };
+      dev = {
+        docker = enabled;
+      };
+    };
+
+    agenix = enabled;
+    impermanence = {
+      enable = true;
+      name = "/persist";
+      directories = [
+        "/var/log"
+        "/var/lib/nixos"
+        "/var/lib/systemd/coredump"
+      ];
+    };
+  };
+
+  system.stateVersion = "24.05";
+}
---
+++ systems/x86_64-linux/xanthus/hardware-configuration.nix
@@ -0,0 +1,74 @@
+{
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  inputs,
+  ...
+}: {
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    inputs.nixos-hardware.nixosModules.lenovo-thinkpad-x1-7th-gen
+  ];
+
+  networking.hostId = "2ed3f4f4";
+
+  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_usb_sdmmc"];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = ["kvm-intel"];
+  boot.extraModulePackages = [];
+
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+    grub = {
+      enable = true;
+      device = "nodev";
+      efiSupport = true;
+      zfsSupport = true;
+    };
+  };
+  boot.initrd.luks.devices.cryptroot.device = "/dev/disk/by-uuid/05a165d9-db38-4714-8d59-49fde6fbc89a";
+
+  boot.initrd.postDeviceCommands = lib.mkAfter ''
+    zfs rollback -r rpool/local/root@blank
+  '';
+
+  fileSystems."/" = {
+    device = "rpool/local/root";
+    fsType = "zfs";
+  };
+
+  fileSystems."/nix" = {
+    device = "rpool/local/nix";
+    fsType = "zfs";
+  };
+
+  fileSystems."/persist" = {
+    device = "rpool/safe/persist";
+    fsType = "zfs";
+    neededForBoot = true;
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/49B1-EE6F";
+    fsType = "vfat";
+    options = ["fmask=0022" "dmask=0022"];
+  };
+
+  swapDevices = [
+    {
+      device = "/dev/zvol/rpool/local/swap";
+      options = ["discard" "0" "1"];
+    }
+  ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}
