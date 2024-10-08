28f813f0a61059a2dd52610dcb1ec71d477f717c
mtaku3 <m.taku3.1222@gmail.com>
2024-10-08 14:01:12 +0900

    fix: fix merging none-ls and none-ls-extra builtins

--- modules/home/app/dev/neovim/config/lua/none-ls/init.lua
+++ modules/home/app/dev/neovim/config/lua/none-ls/init.lua
@@ -13,15 +13,47 @@ M.register_name = none_ls.register_name
 M.reset_sources = none_ls.reset_sources
 M.toggle = none_ls.toggle
 
-M.builtins = vim.tbl_deep_extend("error", none_ls.builtins, {
-  formatting = {
-    ruff = require("none-ls.formatting.ruff"),
-    ruff_format = require("none-ls.formatting.ruff_format"),
-  },
-  diagnostics = {
-    ruff = require("none-ls.diagnostics.ruff"),
-  },
+-- Build builtins metatable
+local logger = require("null-ls.logger")
+
+local export_tables = {
+  diagnostics = {},
+  formatting = {},
+  code_actions = {},
+  hover = {},
+  completion = {},
+  _test = {},
+}
+
+for method, table in pairs(export_tables) do
+  setmetatable(table, {
+    __index = function(t, k)
+      local ok, builtin
+      ok, builtin = pcall(require, string.format("null-ls.builtins.%s.%s", method, k))
+      if not ok then
+        ok, builtin = pcall(require, string.format("none-ls.builtins.%s.%s", method, k))
+        if not ok then
+          logger:warn(string.format("failed to load builtin %s for method %s; please check your config", k, method))
+          return
+        end
+      end
+
+      rawset(t, k, builtin)
+      return builtin
+    end,
+  })
+end
+
+M.builtins = setmetatable(export_tables, {
+  __index = function(t, k)
+    if not rawget(t, k) then
+      logger:warn(string.format("failed to load builtin table for method %s; please check your config", k))
+    end
+
+    return rawget(t, k)
+  end,
 })
+
 M.methods = none_ls.methods
 
 M.formatter = none_ls.formatter
