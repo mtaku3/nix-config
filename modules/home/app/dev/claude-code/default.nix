78488790396d664802fd7a68339bea71c331f04e
mtaku3 <me@mtaku3.com>
2025-09-02 16:57:57 +0900

    fix: some tool available for claude code

--- modules/home/app/dev/claude-code/default.nix
+++ modules/home/app/dev/claude-code/default.nix
@@ -7,13 +7,28 @@
 with lib;
 with lib.capybara; let
   cfg = config.capybara.app.dev.claude-code;
+  claude-code = pkgs.symlinkJoin {
+    name = "claude-code";
+    paths = [pkgs.unstable.claude-code];
+    buildInputs = [pkgs.makeWrapper];
+    postBuild = ''
+      wrapProgram $out/bin/claude \
+        --prefix PATH : ${pkgs.uv}/bin \
+        --prefix PATH : ${pkgs.python312}/bin \
+        --prefix PATH : ${pkgs.nodejs_24}/bin
+    '';
+  };
 in {
   options.capybara.app.dev.claude-code = {
     enable = mkBoolOpt false "Whether to enable the claude-code";
   };
 
   config = mkIf cfg.enable {
-    home.packages = [pkgs.unstable.claude-code pkgs.unstable.repomix pkgs.capybara.superclaude];
+    home.packages = [
+      claude-code
+      pkgs.unstable.repomix
+      pkgs.capybara.superclaude
+    ];
 
     capybara.impermanence.directories = [".claude"];
     capybara.impermanence.files = [".claude.json"];
