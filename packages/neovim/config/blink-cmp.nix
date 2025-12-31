c72e5e6b3fc987dc955350b2415d8d4eca97ca9c
mtaku3 <me@mtaku3.com>
2025-12-31 21:46:42 +0900

    feat: fix cmp and copilot

--- packages/neovim/config/blink-cmp.nix
+++ packages/neovim/config/blink-cmp.nix
@@ -10,6 +10,9 @@
 
       settings = {
         keymap = {
+          "<C-space>" = ["show" "show_documentation" "hide_documentation"];
+          "<C-c>" = ["hide" "fallback"];
+
           "<Tab>" = ["select_next" "snippet_forward" "fallback"];
           "<S-Tab>" = ["select_prev" "snippet_backward" "fallback"];
 
@@ -20,12 +23,29 @@
           "<C-d>" = ["scroll_documentation_down" "fallback"];
 
           "<C-y>" = ["select_and_accept"];
+          "<C-enter>" = ["select_and_accept"];
         };
 
         appearance = {
           use_nvim_cmp_as_default = true;
           nerd_font_variant = "mono";
         };
+
+        completion = {
+          list.selection = {
+            preselect = true;
+            auto_insert = false;
+          };
+          menu.auto_show = false;
+        };
+
+        sources.default = [
+          "copilot"
+          "lsp"
+          "buffer"
+          "snippets"
+          "path"
+        ];
       };
     };
   };
--- packages/neovim/config/copilot.nix
+++ packages/neovim/config/copilot.nix
@@ -5,6 +5,29 @@
   ...
 }: {
   config = {
-    plugins.copilot-vim.enable = true;
+    plugins = {
+      copilot-lua = {
+        enable = true;
+
+        settings = {
+          suggestion.enabled = false;
+          panel.enabled = false;
+
+          filetypes = {
+            markdown = true;
+            help = true;
+          };
+        };
+      };
+
+      blink-copilot.enable = true;
+
+      blink-cmp.settings.sources.providers.copilot = {
+        name = "copilot";
+        module = "blink-copilot";
+        score_offset = 100;
+        async = true;
+      };
+    };
   };
 }
