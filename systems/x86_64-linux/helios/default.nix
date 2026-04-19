17d0d8361ba210425e56e9b4fe46c23bb57ecc52
mtaku3 <me@mtaku3.com>
2026-04-19 23:35:00 +0900

    feat(helios): mount miubiq-fs via sshfs with agenix identity

--- systems/x86_64-linux/helios/default.nix
+++ systems/x86_64-linux/helios/default.nix
@@ -1,6 +1,7 @@
 {
   lib,
   pkgs,
+  config,
   ...
 }:
 with lib;
@@ -36,6 +37,7 @@ with lib.capybara; {
         "/var/log"
         "/var/lib/nixos"
         "/var/lib/systemd/coredump"
+        "/var/lib/sshfs"
       ];
     };
   };
@@ -45,6 +47,39 @@ with lib.capybara; {
   nix.settings.trusted-users = ["mtaku3"];
 
   users.users.root.packages = with pkgs; [git vim curl wget];
+
+  # sshfs mount for miubiq lab filesystem (fs.miubiq.cs.titech.ac.jp:/records)
+  programs.fuse.userAllowOther = true;
+  system.fsPackages = [pkgs.sshfs];
+  age.secrets."sshfs/id_rsa_miubiq_fs" = {
+    mode = "400";
+    owner = "mtaku3";
+    group = "users";
+  };
+  systemd.tmpfiles.rules = [
+    "d /var/lib/sshfs 0700 root root -"
+  ];
+  fileSystems."/mnt/miubiq-fs" = {
+    device = "matsushita@fs.miubiq.cs.titech.ac.jp:/records";
+    fsType = "fuse.sshfs";
+    options = [
+      "allow_other"
+      "default_permissions"
+      "_netdev"
+      "noauto"
+      "x-systemd.automount"
+      "x-systemd.idle-timeout=60"
+      "x-systemd.mount-timeout=30"
+      "reconnect"
+      "ServerAliveInterval=15"
+      "ServerAliveCountMax=3"
+      "port=24322"
+      "IdentityFile=${config.age.secrets."sshfs/id_rsa_miubiq_fs".path}"
+      "UserKnownHostsFile=/var/lib/sshfs/known_hosts"
+      "StrictHostKeyChecking=accept-new"
+      "sftp_server=/usr/lib/openssh/sftp-server\\040-u0"
+    ];
+  };
 
   system.stateVersion = "25.05";
 }
